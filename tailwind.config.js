7fe4aa0933f4caca24e476268e3b495c38660529
Hassan7733 <hsngaml@gmail.com>
2024-03-20 22:08:19 +0200

    button and fonts and primary colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        "primary":"#004A54",
+        "secondry":"F9C02C"
+      }
+    },
   },
   plugins: [],
 }
